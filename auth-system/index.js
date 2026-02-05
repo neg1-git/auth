e988b561cac6e8b6edb3d552489222ec97237ccc
neg1-git <sahilnegi.bt23mech@pec.edu.in>
2026-02-06 02:54:41 +0530

    added login and jwt

--- auth-system/index.js
+++ auth-system/index.js
@@ -3,15 +3,18 @@ const app= express();
 const cors = require('cors');
 const db = require('./db');
 const bcrypt = require('bcrypt');
+const jwtGenerator = require('./utils/jwtGenerator');
 
 app.use(express.json());
 app.use(cors());
 
 app.post('/auth/register',async(req,res)=>{
 
-  const {name}=req.body;
-  const {email}=req.body;
-  const {password}=req.body;
+  // const {name}=req.body;
+  // const {email}=req.body;
+  // const {password}=req.body;
+
+  const {name, email, password}=req.body
 
   const check= await db.query('select * from users where user_email = $1',[email]);
   if(check.rows.length!=0){
@@ -25,11 +28,33 @@ app.post('/auth/register',async(req,res)=>{
   try {
     const result= await db.query('insert into users (user_name, user_email, user_password) values ($1,$2,$3) returning *',[name,email,bcryptPassword])
 
-    res.json(result.rows[0])
+    const token=jwtGenerator(result.rows[0].user_id);
+    res.json({success:true,msg:token})
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
   }
+})
+
+app.post('/auth/login', async(req,res)=>{
+  const {email, plainPassword}=req.body;
+  const result= await db.query('Select * from users where user_email=$1',[email])
+
+  if(result.rows.length===0){
+    return res.status(400).json({success:false,msg:'email doesnt exist.'})
+  }
+
+  const hashedPassword = result.rows[0].user_password
+  const validPassword = await bcrypt.compare(plainPassword,hashedPassword)
+
+  if(!validPassword){
+    return res.status(401).json({success:false,msg:"Password or Email is incorrect"})
+  }
+  else{
+    const token = jwtGenerator(result.rows[0].user_id);
+    return res.status(200).json({success:true,token:token})
+  }
+
 })
 
 
---
+++ auth-system/utils/jwtGenerator.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config(); 
+
+function jwtGenerator(user_id) {
+
+  const payload = {
+    user: user_id
+  };
+
+  console.log("MY SECRET KEY IS:", process.env.jwtSecret);
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "1hr" });
+}
+
+module.exports = jwtGenerator;
